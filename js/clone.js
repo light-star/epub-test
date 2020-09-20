ad8aa99dea23a582ddab2264c17a227fcea4c243
light-star <31688824+light-star@users.noreply.github.com>
2020-09-20 23:00:08 +0800

    Update clone.js
    完成

--- js/clone.js
+++ js/clone.js
@@ -6,8 +6,8 @@
 
 
 
-//这明显是一个深拷贝的问题，并且看到对象中没有值为undefined，function等等数据，所以考虑简易版的深拷贝
-function clone(data){
+//这明显是一个深拷贝的问题，并且看到对象中没有值为undefined，function等等数据，所以考虑简易版的深拷贝 
+let clone=function(data){
   return JSON.parse(JSON.stringify(data));
 }
 //如果有值为undefined和function等值的数据我们可以采用递归的方法遍历对象，对对象中的数组和对象中的对象进行更近一步的处理
